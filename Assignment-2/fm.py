dc530d2887cce8c86d260129b778df6ad2fe5f94
inba <inba2002.p@gmail.com>
2023-02-19 00:47:37 +0530

    working for one pass

--- Assignment-2/fm.py
+++ Assignment-2/fm.py
@@ -51,25 +51,27 @@ def initBucket(adjL, a, b, gain, cut_size, bucket_a, bucket_b):
                 gain[u] -= w
             else:
                 gain[u] += w
-                cut_size += 1
+                cut_size += w
         if u in a:
             bucket_a[gain[u]].add(u)
         else:
             bucket_b[gain[u]].add(u)
+    cut_size /= 2
     print(f"Initial gain: {gain}")
     print(f"Initial cut size: {cut_size}")
     print(f"Initial bucket_a: {bucket_a}")
     print(f"Initial bucket_b: {bucket_b}")
+    return cut_size
 
 
 def get_compliment_set(partition_set_label):
     return 'b' if partition_set_label == 'a' else 'a'
 
 
-def find_maximum_gain_cells(bucket_a, bucket_b, size_a, size_b, MAX_GAIN):
-    min_set = 'a' if size_a < size_b else 'b'
-    size_min_set = eval(f'size_{min_set}')
-    r = size_min_set/(size_a+size_b)
+def find_maximum_gain_cells(bucket_a, bucket_b, size_dict, MAX_GAIN):
+    min_set = 'a' if size_dict['size_a'] < size_dict['size_b'] else 'b'
+    size_min_set = size_dict[f'size_{min_set}']
+    r = size_min_set/(size_dict['size_a']+size_dict['size_b'])
     print(f"ratio: {r}")
     epsilon = 0.01
     if abs(r-0.5) <= epsilon:
@@ -86,29 +88,34 @@ def find_maximum_gain_cells(bucket_a, bucket_b, size_a, size_b, MAX_GAIN):
                 return bucket[g], compliment_set
 
 
-def moveCellAndUpdate(adjL, u, a, b, gain, cut_size, locked_cells, bucket_a, bucket_b, max_gain_set_label):
+def moveCellAndUpdate(adjL, u, a, b, gain, cut_size, bucket_a, bucket_b, lock_cells_set, max_gain_set_label):
+
     for v, w in adjL[u]:
+        if v in lock_cells_set:
+            continue
         old_bucket = eval(f'bucket_{max_gain_set_label}')
         new_bucket = eval(f'bucket_{get_compliment_set(max_gain_set_label)}')
         if inSameSet(u, v, a, b):
             old_bucket[gain[v]].remove(v)
             gain[v] += 2*w
             old_bucket[gain[v]].add(v)
+
         else:
             new_bucket[gain[v]].remove(v)
             gain[v] -= 2*w
             new_bucket[gain[v]].add(v)
 
-    new_cut_size = cut_size - 2*gain[u]
+    print('Cut size before moving cell: ', cut_size)
+    print('Gain of cell before moving cell: ', gain[u])
+    new_cut_size = cut_size - gain[u]
     gain.pop(u)
-    locked_cells.append((u, new_cut_size))
+
     old_cell_set = eval(f'{max_gain_set_label}')
     new_cell_set = eval(f'{get_compliment_set(max_gain_set_label)}')
     old_cell_set.remove(u)
     new_cell_set.add(u)
     print(f"old_cell_set: {old_cell_set}")
     print(f"new_cell_set: {new_cell_set}")
-    print(f"locked_cells: {locked_cells}")
     print(f"gain: {gain}")
     print(f"bucket_a: {bucket_a}")
     print(f"bucket_b: {bucket_b}")
@@ -122,7 +129,9 @@ def rollBackToBestCut(a: Set, b: Set, locked_cells: List, MAX_GAIN):
     stack = list()
     partition_a = a.copy()
     partition_b = b.copy()
-    for idx, u, cut_size in enumerate(locked_cells[::-1]):
+    for idx, (u, cut_size) in enumerate(locked_cells[-1:0:-1]):
+        print(
+            f"Rolling back to cut size: {cut_size}, cell: {u}, idx: {idx},a: {partition_a}, b: {partition_b}")
         min_cut = min(min_cut, (cut_size, idx))
         if u in partition_a:
             partition_a.remove(u)
@@ -132,46 +141,54 @@ def rollBackToBestCut(a: Set, b: Set, locked_cells: List, MAX_GAIN):
             partition_b.remove(u)
             partition_a.add(u)
             stack.append((u, 'a'))
-
-        for _ in range(min_cut[1]):
-            u, partition_label = stack.pop()
-            remove_partition = eval(f'partition_{partition_label}')
-            add_partition = eval(
-                f'partition_{get_compliment_set(partition_label)}')
-            remove_partition.remove(u)
-            add_partition.add(u)
+    print(f"Min cut: {min_cut}")
+    print(f"Rolling back to cut size: {cut_size}")
+    for _ in range(min_cut[1]):
+        u, partition_label = stack.pop()
+        remove_partition = eval(f'partition_{partition_label}')
+        add_partition = eval(
+            f'partition_{get_compliment_set(partition_label)}')
+        remove_partition.remove(u)
+        add_partition.add(u)
     return min_cut, partition_a, partition_b
 
-# adjL, gain, a, b, bucket_a, bucket_b, size_a, size_b, MAX_GAIN, cut_size
+# adjL, gain, a, b, bucket_a, bucket_b, size_dict, MAX_GAIN, cut_size
 
 
 def fmPass(adjL, a, b, area_dict, MAX_GAIN):
-    size_a = sum([area_dict[u] for u in a])
-    size_b = sum([area_dict[u] for u in b])
-    print(f"Initial size_a: {size_a}")
-    print(f"Initial size_b: {size_b}")
+    size_dict = {'size_a': sum([area_dict[u] for u in a]), 'size_b': sum([
+        area_dict[u] for u in b])}
+    print(f"Initial size_dict: {size_dict}")
     gain = defaultdict(int)
     cut_size = 0
     bucket_a = [set() for _ in range(2*MAX_GAIN+1)]
     bucket_b = [set() for _ in range(2*MAX_GAIN+1)]
-    initBucket(adjL, a, b, gain, cut_size, bucket_a, bucket_b)
+    cut_size = initBucket(adjL, a, b, gain, cut_size, bucket_a, bucket_b)
     locked_cells = [(None, cut_size)]
+    lock_cells_set = set()
     while len(locked_cells)-1 < len(adjL.keys()):
         max_gain_set, max_gain_set_label = find_maximum_gain_cells(
-            bucket_a, bucket_b, size_a, size_b, MAX_GAIN)
+            bucket_a, bucket_b, size_dict, MAX_GAIN)
         print(f"max_gain_set: {max_gain_set}")
         print(f"max_gain_set_label: {max_gain_set_label}")
         u = max_gain_set.pop()
         print(
             f"Moving cell {u} from {max_gain_set_label} to {get_compliment_set(max_gain_set_label)}")
         cut_size = moveCellAndUpdate(
-            adjL, u, a, b, gain, cut_size, locked_cells, bucket_a, bucket_b, max_gain_set_label)
+            adjL, u, a, b, gain, cut_size, bucket_a, bucket_b, lock_cells_set, max_gain_set_label)
+        area_dict[u]
+
+        size_dict[f'size_{max_gain_set_label}'] -= area_dict[u]
+        size_dict[f'size_{get_compliment_set(max_gain_set_label)}'] += area_dict[u]
+
         locked_cells.append((u, cut_size))
-    min_cut, best_a, best_b = rollBackToBestCut(a, b, locked_cells)
+        print(f"locked_cells: {locked_cells}")
+        lock_cells_set.add(u)
+    min_cut, best_a, best_b = rollBackToBestCut(a, b, locked_cells, MAX_GAIN)
     print('-'*50)
     print('-'*50)
     print("Completed FM pass")
-    print(f"min_cut: {min_cut}")
+    print(f"min_cut(tuple[Any, int]) : {min_cut}")
     print(f"best_a: {best_a}")
     print(f"best_b: {best_b}")
     print('-'*50)
@@ -182,14 +199,34 @@ def fmPass(adjL, a, b, area_dict, MAX_GAIN):
 def fm(adjM, area_dict, fm_passes=1):
     adjL, nodes, MAX_GAIN = initGraph(adjM)
     a, b = randomPartition(nodes)
+    min_cut = None
     print(f"Partitioning the nodes into two sets.\na->{a}\nb->{b}")
     print('-'*50)
-    best_cut = (MAX_GAIN, a, b)
+    print('-'*50)
     for i in range(fm_passes):
         print('*'*50)
         print('*'*50)
         print('*'*50)
         print(f"FM pass: {i}")
-        best_cut = min(fmPass(adjL, a, b, area_dict, MAX_GAIN), best_cut)
-        a = best_cut[1]
-        b = best_cut[2]
+        min_cut, best_a, best_b = fmPass(adjL, a, b, area_dict, MAX_GAIN)
+        a = best_a
+        b = best_b
+
+    print('#'*50)
+    print('#'*50)
+    print("Final results")
+    print(f"Final min_cut: {min_cut[0]}")
+    print(f"Final a partition: {best_a}")
+    print(f"Final b partition: {best_b}")
+    print('#'*50)
+    print('#'*50)
+
+
+def main():
+    adjM = np.array([[0, 2, 1, 0], [2, 0, 3, 0], [1, 3, 0, 2], [0, 0, 2, 0]])
+    area_dict = {0: 10, 1: 20, 2: 5, 3: 15}
+    fm(adjM, area_dict)
+
+
+if __name__ == "__main__":
+    main()
